1f14980fd9114f00e3b716a94614d8917a191014
jose.alonsogarcia <jose.alonsogarcia@PLX001351010356>
2021-02-17 18:30:01 +0100

    Inicio del proyecto

---
+++ PruebaGithub/PruebaGithub/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba inicio proyecto desde Visual Studio.");
+        }
+    }
+}
