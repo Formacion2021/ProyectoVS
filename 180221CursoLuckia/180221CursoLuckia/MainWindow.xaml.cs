f0f02c551fbac272f24139d5eb7a782689828267
jose.alonsogarcia <jose.alonsogarcia@PLX001351010356>
2021-02-18 13:57:17 +0100

    Añadida calculadora avanzada + Pequeños arreglos.

--- 180221CursoLuckia/180221CursoLuckia/MainWindow.xaml.cs
+++ 180221CursoLuckia/180221CursoLuckia/MainWindow.xaml.cs
@@ -157,7 +157,41 @@ namespace _180221CursoLuckia
 
         private void Btn_decimal_Click(object sender, RoutedEventArgs e)
         {
-            textBoxResultado.Text += ",";
+            if(!textBoxResultado.Text.EndsWith(","))
+            {
+                textBoxResultado.Text += ",";
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Version v_version = new Version();
+            v_version.ShowDialog();
+        }
+
+        private void Btn_Salir_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MenuItemAvanzada_Click(object sender, RoutedEventArgs e)
+        {
+            if (btn_masmenos.Visibility == Visibility.Visible && btn_porcentaje.Visibility == Visibility.Visible
+                && btn_cuadrado.Visibility == Visibility.Visible)
+            {
+                menuItemAvanzada.Header = "Avanzada";
+                btn_masmenos.Visibility = Visibility.Collapsed;
+                btn_porcentaje.Visibility = Visibility.Collapsed;
+                btn_cuadrado.Visibility = Visibility.Collapsed;
+            }
+            else if (btn_masmenos.Visibility == Visibility.Collapsed && btn_porcentaje.Visibility == Visibility.Collapsed
+              && btn_cuadrado.Visibility == Visibility.Collapsed)
+            {
+                menuItemAvanzada.Header = "Simple";
+                btn_masmenos.Visibility = Visibility.Visible;
+                btn_porcentaje.Visibility = Visibility.Visible;
+                btn_cuadrado.Visibility = Visibility.Visible;
+            }
         }
 
         private void Btn_suma_Click(object sender, RoutedEventArgs e)
@@ -168,11 +202,6 @@ namespace _180221CursoLuckia
             resta = false;
             multiplicacion = false;
             division = false;
-        }
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
         }
 
         private void Btn_resta_Click(object sender, RoutedEventArgs e)
@@ -203,6 +232,27 @@ namespace _180221CursoLuckia
             resta = false;
             multiplicacion = false;
             division = true;
+        }
+
+        private void Btn_masmenos_Click(object sender, RoutedEventArgs e)
+        {
+            resAcum = Convert.ToDouble(textBoxResultado.Text);
+            resAcum = resAcum * (-1);
+            textBoxResultado.Text = resAcum.ToString();
+        }
+
+        private void Btn_cuadrado_Click(object sender, RoutedEventArgs e)
+        {
+            resAcum = Convert.ToDouble(textBoxResultado.Text);
+            resAcum = resAcum * resAcum;
+            textBoxResultado.Text = resAcum.ToString();
+        }
+
+        private void Btn_porcentaje_Click(object sender, RoutedEventArgs e)
+        {
+            resAcum = Convert.ToDouble(textBoxResultado.Text);
+            resAcum = resAcum / 100;
+            textBoxResultado.Text = resAcum.ToString();
         }
 
         private void Btn_resultado_Click(object sender, RoutedEventArgs e)
